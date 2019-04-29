70d5c36eec372e65d17af89811e823de7b215891
Joong Gon Yim <joonggonyim@gmail.com>
2019-04-29 00:29:42 -0700

    added KerasCategorical and default_categorical to avoid error. These functions may be removed after refactoring other parts of the code.

--- donkeycar/parts/keras.py
+++ donkeycar/parts/keras.py
@@ -12,7 +12,6 @@ from tensorflow.python.keras.layers import Convolution2D
 from tensorflow.python.keras.layers import Dropout, Flatten, Dense
 from tensorflow.python.keras.callbacks import ModelCheckpoint, EarlyStopping
 
-
 class KerasPilot:
 
     def load(self, model_path):
@@ -58,6 +57,19 @@ class KerasPilot:
             validation_steps=int(steps * (1.0 - train_split) / train_split))
         return hist
 
+class KerasCategorical(KerasPilot):
+    def __init__(self, model=None, *args, **kwargs):
+        super(KerasCategorical, self).__init__(*args, **kwargs)
+        if model:
+            self.model = model
+        else:
+            self.model = default_categorical()
+
+    def run(self, img_arr):
+        img_arr = img_arr.reshape((1,) + img_arr.shape)
+        angle_binned, throttle = self.model.predict(img_arr)
+        angle_unbinned = util.data.linear_unbin(angle_binned[0])
+        return angle_unbinned, throttle[0][0]
 
 class KerasLinear(KerasPilot):
     def __init__(self, model=None, num_outputs=None, *args, **kwargs):
@@ -106,5 +118,42 @@ def default_linear():
                   loss={'angle_out': 'mean_squared_error',
                         'throttle_out': 'mean_squared_error'},
                   loss_weights={'angle_out': 0.5, 'throttle_out': .5})
+
+    return model
+
+def default_categorical():
+    img_in = Input(shape=(120, 160, 3),
+                   name='img_in')  # First layer, input layer, Shape comes from camera.py resolution, RGB
+    x = img_in
+    x = Convolution2D(24, (5, 5), strides=(2, 2), activation='relu')(
+        x)  # 24 features, 5 pixel x 5 pixel kernel (convolution, feauture) window, 2wx2h stride, relu activation
+    x = Convolution2D(32, (5, 5), strides=(2, 2), activation='relu')(
+        x)  # 32 features, 5px5p kernel window, 2wx2h stride, relu activatiion
+    x = Convolution2D(64, (5, 5), strides=(2, 2), activation='relu')(
+        x)  # 64 features, 5px5p kernal window, 2wx2h stride, relu
+    x = Convolution2D(64, (3, 3), strides=(2, 2), activation='relu')(
+        x)  # 64 features, 3px3p kernal window, 2wx2h stride, relu
+    x = Convolution2D(64, (3, 3), strides=(1, 1), activation='relu')(
+        x)  # 64 features, 3px3p kernal window, 1wx1h stride, relu
+
+    # Possibly add MaxPooling (will make it less sensitive to position in image).  Camera angle fixed, so may not to be needed
+
+    x = Flatten(name='flattened')(x)  # Flatten to 1D (Fully connected)
+    x = Dense(100, activation='relu')(x)  # Classify the data into 100 features, make all negatives 0
+    x = Dropout(.1)(x)  # Randomly drop out (turn off) 10% of the neurons (Prevent overfitting)
+    x = Dense(50, activation='relu')(x)  # Classify the data into 50 features, make all negatives 0
+    x = Dropout(.1)(x)  # Randomly drop out 10% of the neurons (Prevent overfitting)
+    # categorical output of the angle
+    angle_out = Dense(15, activation='softmax', name='angle_out')(
+        x)  # Connect every input with every output and output 15 hidden units. Use Softmax to give percentage. 15 categories and find best one based off percentage 0.0-1.0
+
+    # continous output of throttle
+    throttle_out = Dense(1, activation='relu', name='throttle_out')(x)  # Reduce to 1 number, Positive number only
+
+    model = Model(inputs=[img_in], outputs=[angle_out, throttle_out])
+    model.compile(optimizer='adam',
+                  loss={'angle_out': 'categorical_crossentropy',
+                        'throttle_out': 'mean_absolute_error'},
+                  loss_weights={'angle_out': 0.9, 'throttle_out': .01})
 
     return model
